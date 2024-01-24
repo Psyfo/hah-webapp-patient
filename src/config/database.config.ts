88a9984448a204caae4c06213fc5008cf3697161
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-01-24 11:58:50 +0200

    config database

---
+++ src/config/database.config.ts
@@ -0,0 +1,28 @@
+import mongoose, { ConnectOptions } from "mongoose";
+import { logger } from "./logger.config";
+
+// Filename: database.ts
+const connectToDatabase = async () => {
+  const dbUrl: string | undefined =
+    process.env.NODE_ENV === 'production'
+      ? process.env.MONGODB_URI_PROD
+      : process.env.MONGODB_URI_DEV;
+
+  try {
+    await mongoose.connect(
+      dbUrl as string,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions
+    );
+    logger.info('Connected to MongoDB');
+  } catch (err) {
+    logger.error('Failed to connect to MongoDB', err);
+  }
+};
+
+mongoose.set('debug', true); // Log MongoDB query to console
+mongoose.connection.on('error', (error) => logger.error(error)); // Log MongoDB errors to console
+
+export { connectToDatabase };
