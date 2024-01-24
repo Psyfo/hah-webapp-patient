afb0619a0aaa6d06c5f7637e351fedba143b6a8b
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-01-24 11:57:42 +0200

    Add app.ts and package.json

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import * as bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { patientRouter } from "./components/patient/patient.routes";
+import { connectToDatabase } from "./config/database.config";
+import { logger } from "./config/logger.config";
+import { errorHandler } from "./middleware/error.middleware";
+import { router } from "./routes/index.routes";
+
+const app = express();
+dotenv.config();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+express.json();
+express.urlencoded({ extended: true });
+
+// Connect to database
+connectToDatabase();
+
+// Error handling
+app.use(errorHandler);
+
+// Routes
+app.use('/', router); // Use router from index.routes.ts
+app.use('/patient', patientRouter); // Use router from patient.routes.ts
+
+// Start server
+const port = process.env.PORT || 3000; // Use default port if not set
+app.listen(process.env.PORT, () => {
+  logger.info(`Patient microservice listening on port ${process.env.PORT}`);
+});
+//
