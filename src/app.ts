bbd10746b849516826be7773018ca6c30e3c21c0
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-01-29 17:52:51 +0200

    swagger in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import * as bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import { patientRouter } from "./components/patient/patient.routes";
-import { connectToDatabase } from "./config/database.config";
-import { logger } from "./config/logger.config";
-import { errorHandler } from "./middleware/error.middleware";
-import { router } from "./routes/index.routes";
+import * as bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { patientRouter } from './components/patient/patient.routes';
+import { connectToDatabase } from './config/database.config';
+import { logger } from './config/logger.config';
+import { setupSwagger } from './config/swagger.config';
+import { errorHandler } from './middleware/error.middleware';
+import { router } from './routes/index.routes';
 
 const app = express();
 dotenv.config();
@@ -26,7 +27,10 @@ app.use(errorHandler);
 
 // Routes
 app.use('/', router); // Use router from index.routes.ts
-app.use('/patient', patientRouter); // Use router from patient.routes.ts
+app.use('/patients', patientRouter); // Use router from patient.routes.ts
+
+// Swagger
+setupSwagger(app);
 
 // Start server
 const port = process.env.PORT || 3000; // Use default port if not set
