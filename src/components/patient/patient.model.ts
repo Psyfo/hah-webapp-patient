adc6e2711ef7d5883caa28cf77c343a44d34c018
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-01-29 17:50:42 +0200

    create patient model

---
+++ src/components/patient/patient.model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, model } from 'mongoose';
+import { IPatient } from './patient.interface';
+
+const patientSchema = new Schema<IPatient>({
+  date: { type: Date, default: Date.now },
+  firstName: { type: String },
+  lastName: { type: String },
+  gender: { type: String },
+  dob: { type: Date },
+  pic: { type: String },
+  email: { type: String },
+  phoneNumber: { type: String },
+  userName: { type: String },
+  password: { type: String },
+  associatedAccountId: { type: String },
+  associatedAccountRelationship: { type: String },
+  deleted: { type: Boolean, default: false },
+});
+
+const PatientModel = model<IPatient>('Patient', patientSchema);
+
+export { PatientModel };
