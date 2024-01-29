236ed7475b9c3eb2c78749a4091adf05ada534f7
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-01-29 17:52:32 +0200

    swagger description on index route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,22 @@
-import express, { Request, Response } from 'express';
-import { LoggerMiddleware } from '../middleware/logger.middleware';
+import express, { Request, Response } from "express";
+import { LoggerMiddleware } from "../middleware/logger.middleware";
 
 // router for express
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      description: Patient microservice index route
+ *      responses:
+ *         200:
+ *         description: Patient microservice working!
+ *         content:
+ *             application/json:
+ *                example: { "status": 200, "message": "Patient microservice working!" }
+ */
 router.get('/', LoggerMiddleware.reqLog, (req: Request, res: Response) => {
   res.send('Patient microservice working!');
 });
